a181743edbb3cd0a60524d967ba8715bbfc44a1b
Harini12345678 <138786477+Harini12345678@users.noreply.github.com>
2023-07-22 09:22:19 +0530

    Add files via upload

---
+++ class27/Dom.js
@@ -0,0 +1,12 @@
+function check1(){
+    var answerkey= "Jupiter"
+    var answer= document.getElementsById('Planet12').value;
+   // alert(document.getElementsByName('Planet12').checked)
+    var zero = 0
+    alert("harini")
+    if(answerkey === answer)
+    {
+       // alert()
+        zero++
+    }
+} 
